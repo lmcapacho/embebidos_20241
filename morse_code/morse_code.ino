4c50b3efab08df5a0855314bf93fff1c852fd74d
lmcapacho <lmcapacho@gmail.com>
2024-02-13 11:29:34 -0500

    Creación función write para escribir una letra

--- morse_code/morse_code.ino
+++ morse_code/morse_code.ino
@@ -1,3 +1,9 @@
+String morse_array[26] = {".-", "-...", "-.-.", "-..", ".", "..-.",  // A, B, C, D, E, F
+                            "--.", "....", "..", ".---", "-.-", ".-..", // G, H, I, J, K, L
+                            "--", "-.", "---", ".--.", "--.-", ".-.",   // M, N, O, P, Q, R
+                            "...", "-", "..-", "...-", ".--", "-..-",   // S, T, U, V, W, X
+                            "-.--", "--.."};  
+
 void setup()
 {
     pinMode(2, OUTPUT);    
@@ -5,9 +11,25 @@ void setup()
 
 void loop()
 {
-    // Letra A
-    dot();
-    dash();    
+    write('H');
+    write('O');
+    write('L');
+    write('A');    
+}
+
+void write(char c)
+{
+  String code = morse_array[c-65];
+
+  for(int pos = 0; pos < code.length(); pos++ ){
+    if(code.charAt(pos) == '.')
+      dot();
+    else
+      dash();  
+  }
+  delay(3000);
+
+  return 1;
 }
 
 void dot()
