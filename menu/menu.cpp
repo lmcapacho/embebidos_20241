14c112f341deaceda93d76ddbcaf2605f57d6ca8
lmcapacho <lmcapacho@gmail.com>
2024-02-15 09:38:07 -0500

    Nuevo método para agregar items al menú

--- menu/menu.cpp
+++ menu/menu.cpp
@@ -1,5 +1,13 @@
 #include "menu.h"
 
+void Menu::addItem(String text)
+{
+  if(n_items < 10) {
+    items_text[n_items] = text;
+    n_items++;
+  }
+}
+
 void Menu::up()
 {
   if(index < n_items-1){
--- menu/menu.h
+++ menu/menu.h
@@ -5,7 +5,9 @@ class Menu
 private:
   int index; 
   int n_items;
+  String items_text[10];
 public:
+  void addItem(String text);
   void up();
   void down();
 };
